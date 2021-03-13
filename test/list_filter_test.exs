3601d8692f661f155198827caae4d24eee675b7f
llucasmendes <llucasmendes@hotmail.com>
2021-03-13 20:46:18 -0300

    test: list filter test

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -2,7 +2,19 @@ defmodule ListFilterTest do
   use ExUnit.Case
   doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  test "test empty list" do
+    assert ListFilter.call([]) == 0
+  end
+
+  test "test numberlist" do
+    assert ListFilter.call(["1", "2", "3", "4"]) == 2
+  end
+
+  test "test ramdom string list" do
+    assert ListFilter.call(["1", "2", "3", "4", "5", "abc", "xgh"]) == 3
+  end
+
+  test "test ramdom type list" do
+    assert ListFilter.call(["0", 1.1, "2", "3", 4, "5", "abc", :xgh]) == 3
   end
 end
