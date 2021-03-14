b4e9974d6a3f3d148022e445ef3fba1b1a1d8e6f
llucasmendes <llucasmendes@hotmail.com>
2021-03-14 10:16:36 -0300

    chore: remove IO.inspect()

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -6,7 +6,6 @@ defmodule ListFilter do
   defp filter(list) do
     list
     |> Enum.filter(&(!is_atom(&1)))
-    |> IO.inspect()
     |> Enum.map(fn num ->
       case num do
         num when is_integer(num) -> Integer.to_string(num)
@@ -14,20 +13,17 @@ defmodule ListFilter do
         _ -> num
       end
     end)
-    |> IO.inspect()
     |> Enum.flat_map(fn element ->
       case Integer.parse(element) do
         {int, _rest} -> [int]
         :error -> []
       end
     end)
-    |> IO.inspect()
     |> Enum.map(fn element ->
       if rem(element, 2) != 0 do
         element
       end
     end)
-    |> IO.inspect()
     |> Enum.filter(&(!is_nil(&1)))
     |> length()
   end
